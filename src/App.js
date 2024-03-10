a85c83c09df2e111ef0e1fa47b7d9c2ecf37c715
Alaina-gl <108637123+Alaina-gl@users.noreply.github.com>
2024-03-09 19:34:38 -0800

    preliminary frontend webpage with title

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-header">
+      <h1>Find My Mentor</h1>
+    </div>
+  );
+}
+
+export default App;
