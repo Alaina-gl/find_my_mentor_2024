47c57e01c790e838abd87cd27ed2477459f624df
Alaina-gl <108637123+Alaina-gl@users.noreply.github.com>
2024-03-09 16:17:09 -0800

    added node_modules

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=b9eMGE7QtTk&list=PLYHf1Np6LKoKtY21uqd4ctg4iofiWjA9E&index=6" //edited hyperlink here
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
